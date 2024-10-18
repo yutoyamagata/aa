991207f8523d492bab282fb738fe5f1b453d6218
yutoyamagata <sayuto.9120@icloud.com>
2024-10-18 19:22:30 +0900

    Add files via upload

---
+++ 無題5.c
@@ -0,0 +1,12 @@
+int main()
+{   int a;
+    printf("How many students?");
+    scanf("%d", &a);
+    
+    int k;
+    scanf("%d", &k);
+    
+    printf("The number of students is %d", a+k);
+    
+    return 0;
+}
