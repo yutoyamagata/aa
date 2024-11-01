ca180365ed88c7eb3f990f9a6093e36fb02fb098
yutoyamagata <sayuto.9120@icloud.com>
2024-11-01 19:43:28 +0900

    Add files via upload

---
+++ 無題6.c
@@ -0,0 +1,12 @@
+#include <float.h>
+#include <limits.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("char umaximum:%d\n", UCHAR_MAX);
+    printf("char maximum:%d\n", CHAR_MAX);
+    printf("char minimum:%d\n", CHAR_MIN);
+   
+    return 0;
+}
